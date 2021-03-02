a4767c5623722603d71a2941fd3cc3a89c69566d
Marcos Gabriel <56852736+Coronel-Marc@users.noreply.github.com>
2021-03-02 19:17:18 -0300

    iniciando o projeto

---
+++ script/index.js
@@ -0,0 +1,9 @@
+function start(){ 
+        
+        $("#start").hide();
+
+        $("#background").append("<div id='player'></div>");
+        $("#background").append("<div id='enemy1'></div>");
+        $("#background").append("<div id='enemy2'></div>");
+        $("#background").append("<div id='buddy'></div>");
+}
