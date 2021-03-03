30efeb321e0a684d6767102a1b6979a4c697a6a3
Marcos Gabriel <56852736+Coronel-Marc@users.noreply.github.com>
2021-03-03 17:03:27 -0300

    Update index.js
    Todos os elementos agora estão  "andando" pelo cenário.

--- script/index.js
+++ script/index.js
@@ -18,7 +18,10 @@ function start(){
         function loop() {
                 
                 moveBackground();
-                moveplayer(); // função de movimentação do player
+                movePlayer(); // função de movimentação do player
+                moveEnemy1();
+                moveEnemy2();
+                moveBuddy();
 
         } //End of the function loop()
 
@@ -52,15 +55,23 @@ function start(){
                 game.press[e.which] = false;
         });
 
-        function moveplayer() {
+        function movePlayer() {
                 if (game.press[TECLA.W]) {
                         var top = parseInt($("#player").css("top"));
                         $("#player").css("top", top-3);
+
+                        if (top <= 5) {
+                                $("#player").css("top", top+0);
+                        }
                 }
 
                 if (game.press[TECLA.S]) {
                         var top = parseInt($("#player").css("top"));
                         $("#player").css("top", top+3);
+
+                        if (top >= 434) {
+                                $("#player").css("top", top-0);
+                        }
                 }
 
                 if (game.press[TECLA.D]) {
@@ -68,6 +79,45 @@ function start(){
                 }
 
         } //End of the function moveplayer()
+
+        //Movimentação inimiga
+
+        var speed = 2;
+        var positionY = parseInt(Math.random() * 334); 
+
+        function moveEnemy1() {
+                positionX = parseInt($("#enemy1").css("left"));
+                $("#enemy1").css("left",positionX-speed);
+                $("#enemy1").css("top",positionY);
+
+                if (positionX <= -10) {
+                        positionY = parseInt(Math.random() * 334);
+                        $("#enemy1").css("left", 694);
+                        $("#enemy1").css("top", positionY)
+                }
+        } //End Function moveEnemy()
+
+        function moveEnemy2() {
+                positionX = parseInt($("#enemy2").css("left"));
+                $("#enemy2").css("left", positionX-2);
+
+                if (positionX <= 0) {
+                        $("#enemy2").css("left", 775)
+                }
+        }
+
+        function moveBuddy() {
+                positionX = parseFloat($("#buddy").css("left"));
+                $("#buddy").css("left", positionX+0.3);
+
+                if (positionX >= 906) {
+                        $("#buddy").css("left", 0)
+                }
+        }
+
+        
+        
+        
         
 
 } //End of the function start()
